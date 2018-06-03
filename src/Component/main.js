9a26715ce42ad1f8240b3cfc1f0d51913273395a
RaufR <rauf.rahmancz@gmail.com>
2018-06-04 00:12:26 +0200

    component refactoring for next api call

--- src/Component/main.js
+++ src/Component/main.js
@@ -1,39 +1,66 @@
 import React from 'react';
+import '../App.css';
 
 class Main extends React.Component {
-    state = {
-        currencies: [],
-        rate: 0,
-    };
+    constructor(props){
+        super(props);
+        this.state ={
+            currencies: [],
+            value: '?'
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handlePrint = this.handlePrint.bind(this);
+    }
 
-
+    // state = {
+    //     currencies: [],
+    //     value: 'EUR',
+    //     handleChange : this.handleChange.bind(this),
+    //     handlePrint: this.handlePrint.bind(this)
+    // };
     componentDidMount() {
         fetch('https://exchangeratesapi.io/api/latest?symbols=USD,GBP,AUD,JPY')
             .then(data => data.json())
             .then(data => {
                 const currencies = [];
                 currencies.push(data.base, ...Object.entries(data.rates).map(rates => rates[0]));
-                currencies.sort();
-                console.log(currencies);
+
                 console.log(typeof(currencies));
-                this.setState({currencies});
+                this.setState({
+                    currencies
+                });
             })
             .catch(err => console.log(err));
     }
-
      //Dropdown
      DropDown = function(list){
-        return <option>{list}</option>
+        return <option value={list}>{list}</option>
     };
 
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
 
+    handlePrint() {
+        if (this.state.value) {
+            console.log(this.state.value);
+
+        }
+    }
 
     render() {
         const {currencies} = this.state;
         return (
-            <select>
-                {currencies.map(this.DropDown)}
-            </select>
+            <div>
+                <span>SELECT your Base: </span>
+                <select
+                    autoFocus
+                    onChange={this.handleChange}>
+                    <option inputcurrency={currencies} selected data-default>SELECT BASE</option>{currencies.map(this.DropDown)}
+                </select>
+                <button onClick={this.handlePrint}>GET RAtes</button>
+                <p>selected base:{this.handlePrint.value} </p>
+            </div>
         );
     }
 }
